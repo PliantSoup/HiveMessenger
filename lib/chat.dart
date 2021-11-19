35599fdd5754f93ab00a090b4c287ae24052b612
SaltySoup <roman.wowsq@gmail.com>
2021-11-19 16:41:18 +0300

    Чебурек 3

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter95/flutter95.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -19,49 +20,58 @@ class ChatScreen extends StatefulWidget {
 
 class ChatScreenState extends State<ChatScreen> {
   final _controller = TextEditingController();
+  final _scrollController = ScrollController();
   final String username;
-
-  @override
-  void dispose(){
-    super.dispose();
-    ws.sink.close();
-  }
-  @override
-  void initState(){
-    super.initState();
-  }
 
   late var ws;
   late List<Message> messages;
+  bool _needsScroll = false;
+  bool _enableScrollButton = false;
 
-  ChatScreenState(this.username){
+  @override
+  void dispose() {
+    super.dispose();
+    ws.sink.close();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) => _scrollToEnd());
+  }
+
+  _scrollToEnd() async {
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 200), curve: Curves.easeInOut);
+  }
+
+  ChatScreenState(this.username) {
     ws = WebSocketChannel.connect(Uri.parse("ws://euph.ddns.net:1337"));
     messages = [];
     main();
   }
 
-  main() async{
+  main() async {
     ws.sink.add(username);
 
-    ws.stream.listen((m) {
-      final decoded = jsonDecode(m.toString());
+    ws.stream.listen(
+      (m) {
+        final decoded = jsonDecode(m.toString());
 
-      if (decoded["type"] == "message"){
-        messages.add(Message.fromJson(decoded["data"]));
-        setState(() {
-
-        });
-      }
-      if (decoded["type"] == "history"){
-        List? decodedList = decoded["data"];
-        for (int i = 0; i<decodedList!.length; i++){
-          messages.add(Message.fromJson(decodedList[i]));
+        if (decoded["type"] == "message") {
+          messages.add(Message.fromJson(decoded["data"]));
+          _needsScroll = true;
+          setState(() {});
         }
-        setState(() {
-
-        });
-      }
-    },
+        if (decoded["type"] == "history") {
+          List? decodedList = decoded["data"];
+          for (int i = 0; i < decodedList!.length; i++) {
+            messages.add(Message.fromJson(decodedList[i]));
+          }
+          _needsScroll = true;
+          setState(() {});
+        }
+      },
       onDone: () async {
         Fluttertoast.showToast(msg: "Тост из тостера");
       },
@@ -70,8 +80,14 @@ class ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
+    if (_needsScroll) {
+      WidgetsBinding.instance!.addPostFrameCallback((_) => _scrollToEnd());
+      _needsScroll = false;
+    }
     return Scaffold(
-        body: Column(
+        body: Stack(
+          children: [
+          Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             SafeArea(
@@ -81,46 +97,68 @@ class ChatScreenState extends State<ChatScreen> {
               ),
             ),
             Flexible(
-              child: ListView.builder(
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  return MessageBox(author: messages[index].author, text: messages[index].text, time: messages[index].time, color: messages[index].color);
-                }
-              )
-            ),
-            Align(
-              child: Container(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        child: TextField95(
-                          height: 70,
-                          maxLines: 5,
-                          multiline: true,
-                          controller: _controller,
-                        ),
+                child: ListView.builder(
+                    controller: _scrollController,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      return MessageBox(
+                          author: messages[index].author,
+                          text: messages[index].text,
+                          time: messages[index].time,
+                          color: messages[index].color);
+                    })),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 2),
+                      child: TextField95(
+                        height: 70,
+                        maxLines: 5,
+                        multiline: true,
+                        controller: _controller,
                       ),
                     ),
-                    Button95(
-                      padding:
-                        const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
-                      child: const Text("Send"),
-                      onTap: (){
-                        ws.sink.add(_controller.text);
-                        _controller.clear();
-                        setState(() {
-
-                        });
-                      },
-                    )
-                  ],
-                ),
+                  ),
+                  Button95(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+                    child: const Text("Send"),
+                    onTap: () {
+                      if (_controller.text.isNotEmpty) {
+                        if (LineSplitter.split(_controller.text).last.isEmpty) {
+                          String newMessage = _controller.text.trim();
+                          ws.sink.add(newMessage);
+                          _controller.clear();
+                          _scrollToEnd();
+                        } else {
+                          ws.sink.add(_controller.text);
+                          _controller.clear();
+                          _scrollToEnd();
+                        }
+                      }
+                    },
+                  ),
+                ],
               ),
             )
           ],
-        )
-    );
+        ),
+        // Container(
+        //   alignment: Alignment.bottomRight,
+        //   padding: const EdgeInsets.only(right: 5, bottom: 90),
+        //   child: Elevation95(
+        //     child: Button95(
+        //         onTap: () {
+        //           _scrollController
+        //               .jumpTo(_scrollController.position.maxScrollExtent);
+        //         },
+        //         child: const Icon(Icons.navigation)),
+        //   ),
+        // ),
+      ],
+    ));
   }
 }
